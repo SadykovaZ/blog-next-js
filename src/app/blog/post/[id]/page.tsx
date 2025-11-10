da646fe96d5923523b3027610ea2232774a842b4
Sadykova Zhanar <sadykova.zhanar@gmail.com>
2025-11-10 17:57:24 +0500

    fix build

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,12 @@
-import { posts } from '@/app/lib/placeholder-data';
-import Post from '@/app/ui/components/posts/Post';
+import { posts } from "@/app/lib/placeholder-data";
+import Post from "@/app/ui/components/posts/Post";
 
 export default function Page({ params }: { params: { id: string } }) {
   const post = posts.find((post) => post.id === params.id);
-  return (
+  return post ? (
     <>
       <h1>Post</h1>
       <Post {...post} />
-    </>)
+    </>
+  ) : null;
 }
