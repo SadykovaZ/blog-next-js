7d0986d26e0ec0b77e8b4be3b02e827fc19afb74
Sadykova Zhanar <sadykova.zhanar@gmail.com>
2025-11-10 17:55:22 +0500

    Remove .next folder from repository

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/zhanarsadykova/Documents/Zhanar/Learning/blog-next-js/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/47cbc4e2adbc5db9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lyjOazdwZSO0wHFhW3s7vGbRZkb6Uy59d87kUnuOoCQ=\"\n}"
