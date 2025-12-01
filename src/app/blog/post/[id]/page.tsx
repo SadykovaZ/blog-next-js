d2fc8a4a18c1707591628f1de085718fe4717cc6
Sadykova Zhanar <sadykova.zhanar@gmail.com>
2025-12-01 22:21:02 +0500

    remove console logs

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,8 +1,6 @@
 // "use client";
 
-import { useEffect, useState } from "react";
 import { getPosts } from "@/app/lib/data";
-// import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
 import { notFound } from "next/navigation";
 import NotFound from "./not-found";
@@ -15,24 +13,9 @@ export interface PostProps {
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
-  // const [post, setPost] = useState<PostProps | null>();
   const posts = await getPosts();
   const post = posts?.find((post) => post.id === params.id);
 
-  // const loadData = async () => {
-
-  //   setPost(post);
-  // };
-
-  // useEffect(() => {
-  //   loadData();
-  // }, []);
-
-  console.log("====================================");
-  console.log(post, "postspostsposts");
-  console.log("====================================");
-  // const posts = await getPosts();
-  // const post = posts?.find((post) => post.id === params.id);
   if (!post) {
     NotFound();
   }
